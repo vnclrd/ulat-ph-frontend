db7acb8feca91139b270600d72cf3ebda5651a56
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-29 10:10:59 +0800

    Revert "Add framer motion"
    This reverts commit 938247e9c194caa645b00665d373a239777f129c.

--- src/main.jsx
+++ src/main.jsx
@@ -1,58 +1,20 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
-import { AnimatePresence, motion } from "framer-motion"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./index.css"
 import App from "./App.jsx"
 import Core from './components/Core.jsx'
 import { DarkModeProvider } from './components/DarkModeContext.jsx'
 
-function AnimatedRoutes() {
-  const location = useLocation()
-
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        {/* App.jsx */}
-        <Route
-          path="/"
-          element={
-            <motion.div
-              initial={{ x: 0, opacity: 1 }}
-              exit={{ x: -300, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 0.5, ease: "easeInOut" }}
-            >
-              <App />
-            </motion.div>
-          }
-        />
-
-        {/* Core.jsx */}
-        <Route
-          path="/core"
-          element={
-            <motion.div
-              initial={{ x: 300, opacity: 0 }}
-              animate={{ x: 0, opacity: 1 }}
-              exit={{ x: 300, opacity: 0 }}
-              transition={{ duration: 0.5, ease: "easeInOut" }}
-            >
-              <Core />
-            </motion.div>
-          }
-        />
-      </Routes>
-    </AnimatePresence>
-  )
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <DarkModeProvider>
       <BrowserRouter>
-        <AnimatedRoutes />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/core" element={<Core />} />
+        </Routes>
       </BrowserRouter>
     </DarkModeProvider>
   </StrictMode>
-)
+);
