f04107cb765dece114d8e45b99b3190f2ff29ac0
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-29 10:03:23 +0800

    Update animation

--- src/main.jsx
+++ src/main.jsx
@@ -4,24 +4,37 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 import { AnimatePresence, motion } from "framer-motion"
 import "./index.css"
 import App from "./App.jsx"
-import Core from './components/Core.jsx'
-import { DarkModeProvider } from './components/DarkModeContext.jsx'
+import Core from "./components/Core.jsx"
+import { DarkModeProvider } from "./components/DarkModeContext.jsx"
+
+// Page transition variants
+const pageVariants = {
+  initial: { x: 300, opacity: 0 },
+  animate: { x: 0, opacity: 1 },
+  exit: { x: -300, opacity: 0 },
+}
+
+const transition = {
+  duration: 0.35, // slightly faster
+  ease: "easeInOut",
+}
 
 function AnimatedRoutes() {
   const location = useLocation()
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence mode="sync">
       <Routes location={location} key={location.pathname}>
         {/* App.jsx */}
         <Route
           path="/"
           element={
             <motion.div
+              variants={pageVariants}
               initial={{ x: 0, opacity: 1 }}
-              exit={{ x: -300, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 0.5, ease: "easeInOut" }}
+              exit={{ x: -300, opacity: 0 }}
+              transition={transition}
             >
               <App />
             </motion.div>
@@ -33,10 +46,11 @@ function AnimatedRoutes() {
           path="/core"
           element={
             <motion.div
+              variants={pageVariants}
               initial={{ x: 300, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               exit={{ x: 300, opacity: 0 }}
-              transition={{ duration: 0.5, ease: "easeInOut" }}
+              transition={transition}
             >
               <Core />
             </motion.div>
